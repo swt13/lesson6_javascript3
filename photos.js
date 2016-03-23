3d8fa90becd24f2be38fbbf08652dd0de37e1b2b
swt13 <swt13@my.fsu.edu>
2016-03-23 18:18:47 +0000

    lesson 6 final commit

--- photos.js
+++ photos.js
@@ -15,11 +15,20 @@ function populateFigures(){
    var filename;
    var currentFig;
    
+if (figureCount === 3){
    for (var i=1; i<4; i++){
       filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
       currentFig = document.getElementsByTagName("img")[i-1];
       currentFig.src = filename; 
    }// end of for loop 
+} else {
+   for (var i = 0; i<5; i++){
+      filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+      currentFig = document.getElementsByTagName("img")[i];
+      currentFig.src = filename;
+   }// end of for loop
+}// end of else 
+   
 
    
 }// end of populateFigures()
@@ -58,6 +67,8 @@ function leftArrow() {
 //create the fifth image id 
 function previewFive(){
    
+   var articleEl = document.getElementsByTagName("article")[0];
+   
    //create figure and img elements for fifth image
    var lastFigure = document.createElement("figure");
    
@@ -69,7 +80,25 @@ function previewFive(){
    var lastImage = document.createElement("img");
    lastImage.width = "240";
    lastImage.height = "135";
+   
+   lastFigure.appendChild(lastImage);
+   
+   articleEl.appendChild(lastFigure);
+   
+   //clone figure element for fifth image and edit to be first image 
+   var firstFigure = lastFigure.cloneNode(true);
+   
+   firstFigure.id = "fig1";
+   firstFigure.style.right = " ";
+   firstFigure.style.left = "45px";
+   
+   articleEl.insertBefore(firstFigure, document.getElementById("fig2"));
+   
+   // add appropriate src values to two new img elements
+ document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+ document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
 
+   figureCount = 5;
 }//end of previewFive()
 
 
